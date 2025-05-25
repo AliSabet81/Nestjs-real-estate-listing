ad06021b036e35b53a19d93b60e1751514174e8d
AliSabet81 <sabet.farshid1381@gmail.com>
2025-05-25 13:57:50 +0330

    listing images

--- src/modules/listing/listing.service.ts
+++ src/modules/listing/listing.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { ListingProducer } from './queue/listing.producer';
 import { CreateListingDto } from './dto/create-listing.dto';
 import { FileService } from '../../utilities/file/file.service';
 import { DatabaseService } from '../../database/database.service';
+import { UploadListingImageDto } from './dto/upload-listing-image.dto';
+// import { GoogleCloudService } from '../../services/google-cloud/google-cloud.service';
 
 @Injectable()
 export class ListingService {
@@ -11,6 +14,8 @@ export class ListingService {
     private readonly databaseService: DatabaseService,
     private readonly listingQueue: ListingProducer,
     private readonly fileService: FileService,
+    private readonly configService: ConfigService,
+    // private readonly googleCloudService: GoogleCloudService,
   ) {}
 
   async create({
@@ -31,6 +36,29 @@ export class ListingService {
       });
     }
     return listing;
+  }
+
+  async createListingImage(data: UploadListingImageDto) {
+    const mimeType = data.mimeType;
+    const buffer = this.fileService.base64ToBuffer(data.base64File);
+    const bucketName = this.configService.getOrThrow<string>(
+      `gcp.buckets.listingImages`,
+    );
+    // TODO: Uncomment this when if you have a Google Cloud Service
+    // const publicUrl = await this.googleCloudService.uploadFile(
+    //   bucketName,
+    //   buffer,
+    //   mimeType,
+    // );
+    const publicUrl = `https://storage.googleapis.com/${bucketName}/${mimeType}${buffer.length}${Math.random()}`;
+    const listingImage = await this.databaseService.listingImage.create({
+      data: {
+        listingId: data.listingId,
+        url: publicUrl,
+      },
+    });
+    console.log(listingImage);
+    return listingImage;
   }
 
   async findAll() {
--- src/modules/listing/queue/listing.consumer.ts
+++ src/modules/listing/queue/listing.consumer.ts
@@ -1,39 +1,23 @@
 import { Job } from 'bull';
-import { ConfigService } from '@nestjs/config';
 import { Process, Processor } from '@nestjs/bull';
 
+import { ListingService } from '../listing.service';
 import { BaseConsumer } from '../../../core/queue/base.consumer';
-import { FileService } from '../../../utilities/file/file.service';
 import { LoggerService } from '../../../core/logger/logger.service';
 import { LISTING_QUEUE } from '../../../core/queue/queue.constants';
 import { UploadListingImageDto } from '../dto/upload-listing-image.dto';
-import { GoogleCloudService } from '../../../services/google-cloud/google-cloud.service';
 
 @Processor(LISTING_QUEUE)
 export class ListingConsumer extends BaseConsumer {
   constructor(
     logger: LoggerService,
-    private readonly fileService: FileService,
-    private readonly configService: ConfigService,
-    private readonly googleCloudService: GoogleCloudService,
+    private readonly listingService: ListingService,
   ) {
     super(logger);
   }
 
   @Process(`createListingImage`)
   async createListingImage(job: Job<UploadListingImageDto>) {
-    console.log(`createListingImage job data:`, job.data);
-    const mimeType = job.data.mimeType;
-    const buffer = this.fileService.base64ToBuffer(job.data.base64File);
-    const bucketName = this.configService.getOrThrow<string>(
-      `gcp.buckets.listingImages`,
-    );
-    const publicUrl = await this.googleCloudService.uploadFile(
-      bucketName,
-      buffer,
-      mimeType,
-    );
-    console.log(`Public image file`, publicUrl);
-    // TODO: store respective Google Cloud Storage URL in database
+    return await this.listingService.createListingImage(job.data);
   }
 }
